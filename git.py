ad71298688aff828fa7aa583a1c9a1b70e39e99b
Arshia Mohammadi <arshiamoh123@gmail.com>
2021-02-12 00:58:15 +0330

    added the new function to the new branch

--- git.py
+++ git.py
@@ -2,10 +2,13 @@
 def main():
     print("the main is called")
 
+def feature():
+    print("this is only available on feature branch")
 
 
 if __name__ == "__main__":
     main()
+    feature()
 
 
 
